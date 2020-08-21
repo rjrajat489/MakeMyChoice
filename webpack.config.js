5182bae2a867cfa229c1375467332d55f811aaa7
RAJAT SAHA <rajat.12.saha1995@gmail.com>
2020-08-21 21:56:51 +0530

    Getting ready for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,56 @@
-const path= require('path');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-module.exports={
-entry: './src/app.js',
-output: {
-    path: path.join(__dirname,'public'),
-    filename: 'bundle.js'
-},
-module:{
-    rules:[{
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract=new MiniCssExtractPlugin()
+  return {
+    entry: ['babel-polyfill', './src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
         loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
-    },{
-        test:/\.scss$/,
-        use:[
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]
+      }, {
+        test: /\.s?css$/,
+        use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                esModule: true,
+              },
+            },
+            {
+              loader:'css-loader',
+              options:{
+                sourceMap:true
+              }
+            
+            },
+            {
+              loader:'sass-loader',
+              options:{
+                sourceMap:true
+              } 
+            }
+          ],
+      }
+    ]
+    },
+    plugins: [
+      CSSExtract,
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-]
-},
-devtool:'cheap-module-eval-source-map',
-devServer:{
-    contentBase: path.join(__dirname,'public')
-}
+  };
 };
